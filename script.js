f431e196d70728d98d3df710961bb40a0cdca237
Matt <matthewsantos2403@gmail.com>
2025-09-11 23:06:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -155,25 +155,4 @@ counterInput.addEventListener('keypress', function(e) {
   }
 });
 
-function Url1() {
-    window.location.href = "https://github.com/bismarck017";
-}
-
-function Url2() {
-    window.location.href = "https://discord.com/invite/gWwJ5SQq4g";
-}
-
-function Url3() {
-    window.location.href = "missiles.html";
-}
-
-function Url4() {
-    window.location.href = "counters.html";
-}
-
-function Url5() {
-    window.location.href = "home.html";
-
-}
-
 displayNewMissile();
